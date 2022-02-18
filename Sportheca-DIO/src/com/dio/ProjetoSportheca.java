f7a4438c54fcfa57f22e565798bfe8aa0aee2035
zoegod <zoegod@pentest.zoegod>
2022-02-18 16:17:59 -0300

    adicionando arquivos no github

---
+++ Sportheca-DIO/src/com/dio/ProjetoSportheca.java
@@ -0,0 +1,25 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class ProjetoSportheca {
+
+	public static void main(String[] args) {
+	
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+	System.out.println(gato);
+		/*int a = 2;
+		int b = 3;
+System.out.println("Olá Mundo! " + (a+b));*/
+	}
+
+}
+
+class Livros {
+	
+private String nome;
+private String npags;
+}
+
